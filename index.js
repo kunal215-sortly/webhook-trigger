0e91e50eab5db977b970b484980db0774d9aa676
kunal215-sortly <kunal@sortlyapp.com>
2025-12-15 22:43:21 +0530

    Initial

--- index.js
+++ index.js
@@ -37,8 +37,13 @@ app.post('/invoke-webhook', async (req, res) => {
   }
 });
 
+const shouldUseKunalToken = true;
+
+const token = shouldUseKunalToken ? process.env.KUNAL_ACCESS_TOKEN : process.env.ASHWIN_ACCESS_TOKEN;
+const companyId = shouldUseKunalToken ? process.env.KUNAL_COMPANY_ID : process.env.ASHWIN_COMPANY_ID;
+
 // POST /create-job
-app.post('/create-job', (req, res) => {
+app.post('/create-job', async (req, res) => {
   console.log('--- Incoming /create-job request ---');
   console.log('Headers:', req.headers);
   console.log('Query:', req.query);
@@ -46,7 +51,51 @@ app.post('/create-job', (req, res) => {
   console.log('Method:', req.method);
   console.log('URL:', req.originalUrl);
   console.log('-----------------------------------');
-  res.status(200).send('Payload received.');
+
+  if (req.body.text) {
+    try {
+      const apiResponse = await fetch(`https://api.sortly.com/v2/companies/${companyId}/nodes`, {
+        method: 'POST',
+        headers: {
+          'accept': 'application/json',
+          'authorization': `Bearer ${token}`,
+          'accept-language': 'en-US,en;q=0.9,hi;q=0.8',
+          'cache-control': 'no-cache',
+            'content-type': 'application/json',
+            'Referer': 'https://app-qa.sortly.co/',
+            'sec-ch-ua': '"Google Chrome";v="143", "Chromium";v="143", "Not A(Brand";v="24"',
+            'sec-ch-ua-mobile': '?0',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36'
+        },
+        body: JSON.stringify({
+          "include_acl": true,
+          "node": {
+            "id": "0d1b2732-d53f-4eb5-93d4-fe21095a1be3",
+            "parent_id": null,
+            "is_item": false,
+            "name": req.body.text,
+            "notes": null,
+            "tags": [],
+            "photo_ids": [],
+            "label_url": null,
+            "label_url_type": null,
+            "label_url_extra": null,
+            "label_url_extra_type": null,
+            "label_attributes": null,
+            "custom_attribute_values": [],
+            "notification_triggers": [],
+            "origin_folder_id": null
+          }
+        })
+      });
+      const apiResult = await apiResponse.json().catch(() => ({}));
+      res.status(200).json({ message: 'Successfully created folder', apiResult });
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to call Sortly API', details: err.message });
+    }
+  } else {
+    res.status(200).send('Payload received.');
+  }
 });
 
 const PORT = process.env.PORT || 3000;
